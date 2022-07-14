e713c17176748aab194f5b24db29587895a83159
野坂匠吾 <nosakatakumiware@wlan-napt-005.u-aizu.ac.jp>
2022-07-14 19:12:19 +0900

    first commit

--- coin.java
+++ coin.java
@@ -1,15 +1,9 @@
 import java.util.Random;
-import java.util.Scanner;
 class coin {
   public static void main(String args[]) {
       int h=0;
       int t=0;
       int f=0;
-      System.out.println("Who are you?");
-      Scanner scan = new Scanner(System.in);
-        String str = scan.nextLine();
-        scan.close();
-        System.out.println("Hello,"+str+"!");
     System.out.println("Tossing a coin...");
       for(int i=1;i<=3;i++){
     Random rand = new Random();
@@ -25,14 +19,8 @@ class coin {
         
   }
   System.out.println("Heads:"+h+",Tails:"+t);
-  if(h>t){
-    System.out.println(str+" won!");
-  }
-  else{
-    System.out.println(str+" lost!");
-  }
-
   
+
 
 
 }
